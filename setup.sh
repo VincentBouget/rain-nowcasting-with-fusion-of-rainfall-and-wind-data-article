906577c044dd4c69d78d7082d5d106cb51d8cf71
Dominique Béréziat <Dominique.Bereziat@gmail.com>
2020-12-15 14:38:28 +0100

    enhance portability of setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 # secure shell
 set -e -u  
 
-which -s python3 || { echo "Error: python3 required"; exit 1; }
-which -s wget || { echo "Error: wget required"; exit 1; }
+which >/dev/null python3 || { echo "Error: python3 required"; exit 1; }
+which >/dev/null wget || { echo "Error: wget required"; exit 1; }
 
 echo "Install python3 env (setupenv)"
 python3 -m venv setupenv
@@ -18,6 +18,6 @@ rm -f /tmp/requirements.txt
 pip install jupyter
 
 echo "Download dataset"
-wget https://www-pequan.lip6.fr/~bereziat/rain-nowcasting/data.tar.gz -O - | tar xvf -
+wget --no-check-certificate https://www-pequan.lip6.fr/~bereziat/rain-nowcasting/data.tar.gz -O - | tar xvf -
 
 
