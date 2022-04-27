08f5aad07848ad3b10989687074cfce573078c76
Dominique Béréziat <dominique.bereziat@free.fr>
2022-04-27 10:40:18 +0200

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# All right reserved by Vincent Bouget, Arthur Filoche, Anastase Charantonis, Dominique Béréziat, Julien Brajard
+# All rights reserved by Vincent Bouget, Arthur Filoche, Anastase Charantonis, Dominique Béréziat, Julien Brajard
 # A word funded by Sorbonne Center for Artificial Intelligence (Sorbonne Université)
 
 import torch
